e27a5adfe8ee08eb147b324a5d984f0028abbac7
Saeka03 <143541044+Saeka03@users.noreply.github.com>
2024-11-29 05:27:42 -0800

    Implement the header (#2)

---
+++ frontend/src/app/components/Header.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import React from "react";
+import styles from "./Header.module.scss";
+import { Yellowtail } from "next/font/google";
+
+const yellowtail = Yellowtail({
+  subsets: ["latin"], 
+  weight: "400", 
+});
+
+function Header() {
+  return (
+    <div className={styles.header}>
+      <Image
+        className={styles.img}
+        src={"/img-header.png"}
+        alt="Header image"
+        height={150}
+        width={2500}
+      />
+      <h1 className={`${styles.title} ${yellowtail.className}`}>
+        No Name Diary
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from "./page.module.scss";
+import Header from "./components/Header";
 
 function Page() {
-  return <div className={styles.title}>no name diary</div>;
+  return (
+    <div>
+      <Header />
+      <div className={styles.title}>no name diary</div>
+    </div>
+  );
 }
 
 export default Page;
