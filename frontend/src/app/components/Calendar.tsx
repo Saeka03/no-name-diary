faad63685e6325feb20615fa10a139cc5ea280a2
Saeka Kubota <hdr23.nknkn@gmail.com>
2025-01-02 04:31:35 -0800

    Fix the plus button

--- frontend/src/app/components/Calendar.tsx
+++ frontend/src/app/components/Calendar.tsx
@@ -6,6 +6,7 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import styles from "./Calendar.module.scss";
+import Modal from "./Modal";
 
 function Calendar() {
   return (
@@ -20,11 +21,24 @@ function Calendar() {
           start: "title",
           end: "prev  today next",
         }}
+        dayCellDidMount={(info) => {
+          // Create the "+" button
+          const plusButton = document.createElement("div");
+          plusButton.className = styles.plus;
+          plusButton.textContent = "+";
+
+          // Append it to the day cell
+          const dayGridFrame = info.el.querySelector(".fc-daygrid-day-frame");
+          if (dayGridFrame) {
+            dayGridFrame.appendChild(plusButton);
+          }
+        }}
         events={[
-          { title: "Event 1", date: "2024-12-01" },
-          { title: "Event 2", date: "2024-12-05" },
+          { title: "Event 1", date: "2025-01-31" },
+          { title: "Event 2", date: "2025-01-25" },
         ]}
       />
+      <div className={styles.modal}>{/* <Modal /> */}</div>
     </div>
   );
 }
