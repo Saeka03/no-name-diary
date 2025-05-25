b0d5ee7b53b26870c243ce56cbb42d4f20a351bf
Saeka Kubota <hdr23.nknkn@gmail.com>
2025-05-25 13:49:38 -0700

    Restructure the database

--- frontend/src/seed/dummy.ts
+++ frontend/src/seed/dummy.ts
@@ -3,52 +3,31 @@ import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function insertAdmin() {
-  await prisma.admin.create({
+async function insertUser() {
+  await prisma.user.create({
     data: {
-      name: "Sachiko03",
-      password: "sachiko",
+      id: "f558cd5c-af2d-4fe1-a106-4d1d5dfa747d",
+      diaries: {
+        create: [
+          {
+            dateTime: new Date(),
+            title: "Roboto",
+            content: "blah blah blah",
+            like: 1,
+            laugh: 2,
+            cry: 3,
+          },
+          {
+            dateTime: new Date(),
+            title: "Poppins",
+            content: "blah blah blah",
+            like: 0,
+            laugh: 0,
+            cry: 4,
+          },
+        ],
+      },
     },
-  });
-}
-
-async function insertDiary() {
-  await prisma.diary.createMany({
-    data: [
-      {
-        dateTime: new Date(),
-        title: "Roboto",
-        content: "blah blah blah",
-        like: 1,
-        laugh: 2,
-        cry: 3,
-      },
-      {
-        dateTime: new Date(),
-        title: "Poppins",
-        content: "blah blah blah",
-        like: 0,
-        laugh: 0,
-        cry: 4,
-      },
-    ],
-  });
-}
-
-async function insertComments() {
-  await prisma.comment.createMany({
-    data: [
-      {
-        dateTime: new Date(),
-        content: "You're right.",
-        diaryId: 1,
-      },
-      {
-        dateTime: new Date(),
-        content: "I know right.",
-        diaryId: 1,
-      },
-    ],
   });
 }
 
@@ -59,16 +38,13 @@ async function deleteAll(table: Uncapitalize<Prisma.ModelName>) {
 
 async function main() {
   {
-    await deleteAll("comment");
     await deleteAll("diary");
-    await deleteAll("admin");
+    await deleteAll("user");
   }
 
   {
     // Independent
-    await insertAdmin();
-    await insertDiary();
-    await insertComments();
+    await insertUser();
   }
 }
 
