50009c4f1512427a5e63a75b8ac43cbcb57a327b
Saeka Kubota <hdr23.nknkn@gmail.com>
2025-06-01 00:37:13 -0700

    Use supabase to handle db data

--- frontend/src/stores/diaryStore.ts
+++ frontend/src/stores/diaryStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { supabase } from "../lib/supabase";
 
 interface DiariesState {
   diaries: {
@@ -31,7 +32,6 @@ interface DiariesState {
     content: string,
     adminId: string
   ) => Promise<void>;
-  getDiary: (id: string) => Promise<void>;
   deleteDiary: (id: string) => Promise<void>;
   editDiary: (id, title, content) => Promise<void>;
   incrementReaction: (id, type) => Promise<void>;
@@ -43,15 +43,19 @@ export const useDiariesStore = create<DiariesState>()((set) => ({
   diary: null,
   fetchDiaries: async () => {
     try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_FRONTEND_ORIGIN}/api/diaries`
-      );
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
+      // const response = await fetch(
+      //   `${process.env.NEXT_PUBLIC_FRONTEND_ORIGIN}/api/diaries`
+      // );
+      // if (!response.ok) {
+      //   throw new Error(`HTTP error! status: ${response.status}`);
+      // }
+      // const data = await response.json();
+
+      const { data, error } = await supabase.from("Diary").select("*");
+      if (error) console.log(error.message);
+
       set({
-        diaries: data.diaries.map((diary) => ({
+        diaries: data.map((diary) => ({
           id: diary.id.toString(),
           title: diary.title,
           date: diary.dateTime,
@@ -69,24 +73,32 @@ export const useDiariesStore = create<DiariesState>()((set) => ({
   },
   fetchDiary: async (id) => {
     try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_FRONTEND_ORIGIN}/api/diaries/${id}`
-      );
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
+      // const response = await fetch(
+      //   `${process.env.NEXT_PUBLIC_FRONTEND_ORIGIN}/api/diaries/${id}`
+      // );
+      // if (!response.ok) {
+      //   throw new Error(`HTTP error! status: ${response.status}`);
+      // }
+      // const data = await response.json();
+
+      const { data, error } = await supabase
+        .from("Diary")
+        .select()
+        .eq("id", id)
+        .single();
+      if (error) console.log(error.message);
+
       set({
         diary: {
-          id: data.diary.id.toString(),
-          title: data.diary.title,
-          date: data.diary.dateTime,
-          content: data.diary.content,
-          like: data.diary.like,
-          laugh: data.diary.laugh,
-          cry: data.diary.cry,
-          comment: data.diary.comment,
-          adminId: data.diary.adminId,
+          id: data.id.toString(),
+          title: data.title,
+          date: data.dateTime,
+          content: data.content,
+          like: data.like,
+          laugh: data.laugh,
+          cry: data.cry,
+          comment: data.comment,
+          adminId: data.adminId,
         },
       });
     } catch (error) {
@@ -95,85 +107,111 @@ export const useDiariesStore = create<DiariesState>()((set) => ({
   },
   postDiary: async (dateTime, title, content, adminId) => {
     try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_FRONTEND_ORIGIN}/api/diaries`,
-        {
-          method: "POST",
-          body: JSON.stringify({ dateTime, title, content, adminId }),
-        }
-      );
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error(error);
-    }
-  },
-  getDiary: async (id) => {
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_FRONTEND_ORIGIN}/api/diaries/${id}`
-      );
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      return data;
+      // const response = await fetch(
+      //   `${process.env.NEXT_PUBLIC_FRONTEND_ORIGIN}/api/diaries`,
+      //   {
+      //     method: "POST",
+      //     body: JSON.stringify({ dateTime, title, content, adminId }),
+      //   }
+      // );
+      // if (!response.ok) {
+      //   throw new Error(`HTTP error! status: ${response.status}`);
+      // }
+      // const data = await response.json();
+      // return data;
+
+      const { error } = await supabase
+        .from("Diary")
+        .insert([
+          { dateTime, title, content, adminId, like: 0, laugh: 0, cry: 0 },
+        ])
+        .select();
+      if (error) console.log(error.message);
     } catch (error) {
       console.error(error);
     }
   },
   deleteDiary: async (id) => {
     try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_FRONTEND_ORIGIN}/api/diaries/${id}`,
-        {
-          method: "DELETE",
-        }
-      );
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      return data;
+      // const response = await fetch(
+      //   `${process.env.NEXT_PUBLIC_FRONTEND_ORIGIN}/api/diaries/${id}`,
+      //   {
+      //     method: "DELETE",
+      //   }
+      // );
+      // if (!response.ok) {
+      //   throw new Error(`HTTP error! status: ${response.status}`);
+      // }
+      // const data = await response.json();
+      // return data;
+
+      const { error } = await supabase.from("Diary").delete().eq("id", id);
+      if (error) console.log(error.message);
     } catch (error) {
       console.error(error);
     }
   },
   editDiary: async (id, title, content) => {
     try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_FRONTEND_ORIGIN}/api/diaries/${id}`,
-        {
-          method: "PUT",
-          body: JSON.stringify({ title, content }),
-        }
-      );
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      return data;
+      // const response = await fetch(
+      //   `${process.env.NEXT_PUBLIC_FRONTEND_ORIGIN}/api/diaries/${id}`,
+      //   {
+      //     method: "PUT",
+      //     body: JSON.stringify({ title, content }),
+      //   }
+      // );
+      // if (!response.ok) {
+      //   throw new Error(`HTTP error! status: ${response.status}`);
+      // }
+      // const data = await response.json();
+      // return data;
+
+      const { error } = await supabase
+        .from("Diary")
+        .update({ title, content })
+        .eq("id", id)
+        .select();
+      if (error) console.log(error.message);
     } catch (error) {
       console.error(error);
     }
   },
   incrementReaction: async (id, type) => {
     try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_FRONTEND_ORIGIN}/api/diaries/${id}`,
-        {
-          method: "PATCH",
-          body: JSON.stringify({ type }),
-        }
-      );
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+      // const response = await fetch(
+      //   `${process.env.NEXT_PUBLIC_FRONTEND_ORIGIN}/api/diaries/${id}`,
+      //   {
+      //     method: "PATCH",
+      //     body: JSON.stringify({ type }),
+      //   }
+      // );
+      // if (!response.ok) {
+      //   throw new Error(`HTTP error! status: ${response.status}`);
+      // }
+      // const data = await response.json();
+      // return data;
+      if (type === "like") {
+        const { error } = await supabase.rpc("increment", {
+          x: 1,
+          row_id: id,
+          column_name: "like",
+        });
+        if (error) console.log(error.message);
+      } else if (type === "laugh") {
+        const { error } = await supabase.rpc("increment", {
+          x: 1,
+          row_id: id,
+          column_name: "laugh",
+        });
+        if (error) console.log(error.message);
+      } else if (type === "cry") {
+        const { error } = await supabase.rpc("increment", {
+          x: 1,
+          row_id: id,
+          column_name: "cry",
+        });
+        if (error) console.log(error.message);
       }
-      const data = await response.json();
-      return data;
     } catch (error) {
       console.error(error);
     }
